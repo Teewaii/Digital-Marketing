30ec33828975d49a83bdc56226ee83eb4e5fbe30
Taiye Ajimati <taiyeajimati@gmail.com>
2022-08-21 18:20:23 +0400

    last office commit

--- src/components/Value/Value.jsx
+++ src/components/Value/Value.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import './Value.css'
+import secure from '../../images/Secure.png'
+import cookies from '../../images/Cookies.png'
+import login from '../../images/Login_NotRequired.png'
+import MoneyBack from '../../images/Money_Back.png'
 
 export default function Value() {
     return (
@@ -9,26 +13,33 @@ export default function Value() {
                     One platform to increase followers on all social media.
                 </h1>
                 <div className="values">
-                    <div className="secure">
-                        <img src="" alt="" />
+                    <div className="secure card">
+                        <img src={secure} alt="secure icon" />
                         <h1 className='caption'>100% Secure</h1>
                         <p className="body">From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly.</p>
                     </div>
-                    <div className="cookies">
-                        <img src="" alt="" />
+                    <div className="cookies card">
+                        <img src={cookies} alt="cookies-icon" />
                         <h1 className='caption'>No Cookies Required</h1>
                         <p className="body">Age and draw mrs like. Improving end distrusts may instantly was household applauded incommode.</p>
                     </div>
-                    <div className="login">
-                        <img src="" alt="" />
+                    <div className="login card">
+                        <img src={login} alt="login-icon" />
                         <h1 className='caption'>Login Info Not Required</h1>
                         <p className="body">Draw mrs like. Improving end distrusts may instantly was household applauded incommode.</p>
                     </div>
-                    <div className="guarantee">
-                        <img src="" alt="" />
+                    <div className="guarantee card">
+                        <img src={MoneyBack} alt="MoneyBack-icon" />
                         <h1 className='caption'>Moneyback Guarrentee</h1>
                         <p className="body">Improving end distrusts may instantly was from they fine john he give of rich he. They age and draw mrs like. </p>
                     </div>
+                </div>
+            </div>
+            <div className="bottom-container">
+                <div className="description">
+                    <h1 className="head">Get thousands of active followers</h1>
+                    <p className="body">Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment.</p>
+                    <button className='btn'>Start Free</button>
                 </div>
             </div>
         </section>
