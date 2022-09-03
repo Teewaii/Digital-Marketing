77ea535b47dae24381d8e0ea3501da1f45089630
Taiye <taiyeajimati@gmail.com>
2022-09-03 12:35:52 +0400

    Footer section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Clients from './components/Clients/Clients';
 import FAQ from './components/FAQ/FAQ';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Subscribe from './components/Subscribe/Subscribe';
 import Testimonials from './components/Testimonials/Testimonials';
@@ -27,6 +28,7 @@ function App() {
       <FAQ />
       <Subscribe />
       <FAQ />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import './Footer.css';
+import { HiArrowSmRight } from 'react-icons/hi'
+export default function Footer() {
+    return (
+        <section className="footer-wrapper ">
+            <div className="footer container">
+                <div className="top-section">
+                    <h1 className="head">Join millions of
+                        creative people with Shakir</h1>
+                    <button className='btn waitlist'>Join the waitlist<span><HiArrowSmRight /></span></button>
+                </div>
+                <div className="middle-section">
+                    <div className="leftSide">
+                        <h1 className="head">Teewaii</h1>
+                        <p className="body">Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment.</p>
+                    </div>
+                    <div className="rightSide">
+                        <ul className="company">
+                            <h4 className="title">Company</h4>
+                            <li><a href='#'>About Us</a></li>
+                            <li><a href='#'>Careers</a></li>
+                            <li><a href='#'>Blog</a></li>
+                            <li><a href='#'>Pricing</a></li>
+                        </ul>
+                        <ul className="product">
+                            <h4 className="title">Product</h4>
+                            <li><a href='#'>Facebook followers</a></li>
+                            <li><a href='#'>Instagram Followers</a></li>
+                            <li><a href='#'>Twitter Followers</a></li>
+                            <li><a href='#'>TikTok Followers</a></li>
+                        </ul>
+                        <ul className="resources">
+                            <h4 className="title">Resources</h4>
+                            <li><a href='#'>Proposal Template</a></li>
+                            <li><a href='#'>Invoices</a></li>
+                            <li><a href='#'>Tuturoial</a></li>
+                            <li><a href='#'>How to increase activity</a></li>
+                        </ul>
+                    </div>
+                    <div className="bottom-section">
+                        <p className="body">Developed by <a href='https://github.com/Teewaii'>Teewai</a></p>
+                        <p className="body art">Designed by ARShakir, Inc. All rights reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
